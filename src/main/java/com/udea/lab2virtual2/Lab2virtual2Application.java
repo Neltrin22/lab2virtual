bd1e573e2d841db6a4f7afa66adf3b10a265dba4
Nelson Puerta <alcides.puerta@udea.edu.co>
2024-10-05 08:27:45 -0500

    Primer Commit

---
+++ src/main/java/com/udea/lab2virtual2/Lab2virtual2Application.java
@@ -0,0 +1,13 @@
+package com.udea.lab2virtual2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2virtual2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2virtual2Application.class, args);
+	}
+
+}
